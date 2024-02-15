f9077f3f55d70170e79832820f11c3d653285c45
Oly Graterol <olygraterolc@gmail.com>
2024-02-15 15:24:00 -0400

    actualizacion para produccion

--- public/build/assets/InputLabel-K0sPL1za.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,t as o,p as l}from"./app-im3r4HLh.js";const n={class:"block text-sm text-gray-700"},c={key:0},r={key:1},u={__name:"InputLabel",props:["value"],setup(s){return(a,_)=>(e(),t("label",n,[s.value?(e(),t("span",c,o(s.value),1)):(e(),t("span",r,[l(a.$slots,"default")]))]))}};export{u as _};
